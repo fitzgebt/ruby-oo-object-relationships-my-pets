1e629e01da8cf64d429c9d1c24d0600a662003e8
fitzgebt <fitzgebt@gmail.com>
2021-01-25 16:51:16 -0800

    commit tree 6a910844c1d394d4956f062c14fb835850672008

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_writer
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @species=species
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
